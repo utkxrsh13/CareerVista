44ba129ba366473293e79f2aec451be3a22174e5
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-28 00:16:46 +0530

    fewav

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://careervista-frontend.onrender.com',
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true
 }
 
